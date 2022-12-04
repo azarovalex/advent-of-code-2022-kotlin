b5b45f4b21be5c8f60c94e04defbe766a18d9a91
Alex Azarov <alex@azarov.by>
2022-12-04 23:22:03 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
+    fun groupLines(input: List<String>): List<Int> {
+        return input.fold(listOf(0)) { array, element ->
+            if (element.isEmpty()) {
+                array + listOf(0)
+            } else {
+                array.dropLast(1) + listOf(array.last() + element.toInt())
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return groupLines(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return groupLines(input)
+            .sortedDescending()
+            .take(3)
+            .reduce { a, b -> a + b }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
